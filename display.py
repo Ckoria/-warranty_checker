48e7d73b79d0738cd4ce47e1df7d8b456fcd90e2
Chris Black <31255096+Ckoria@users.noreply.github.com>
2023-06-04 13:11:20 +0200

    Update display.py

--- display.py
+++ display.py
@@ -58,7 +58,7 @@ def visuals():
             date = res.get("produced_date")
             produced_date = (datetime(int(date[0:4]),int(date[4:6]),int(date[6:])))
             st.write(f'Manufactured on')
-            st.write(( {str(produced_date)[0:10]))
+            st.write((str(produced_date)[0:10]))
         xpand = st.expander("Disclaimer")
         xpand.write('Please note that this app can only be used for project demonstration.  It is not authorised by Samsung.')
     except:
