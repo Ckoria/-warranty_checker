47f1e45316618d94fca622c7ee31275cc2e5dbbd
Chris Black <31255096+Ckoria@users.noreply.github.com>
2023-06-04 12:49:33 +0200

    Update display.py

--- display.py
+++ display.py
@@ -52,12 +52,12 @@ def visuals():
             else:
                 st.write(f"Ooops! No longer in warranty. Expired on {str(datetime.now() - date)[-13:-1]} ago.")
         with col3:
-            wrty_date = f"Warranty expires on {(date).strftime("%c")}"
+            wrty_date = f"Warranty expires on {(date).strftime("%x")}"
             st.write(wrty_date)
         with col4:
             date = res.get("produced_date")
             produced_date = (datetime(int(date[0:4]),int(date[4:6]),int(date[6:])))
-            p_date = f'Manufactured on {produced_date.strftime("%c")}'
+            p_date = f'Manufactured on {produced_date.strftime("%x")}'
             st.write((p_date))
         xpand = st.expander("Disclaimer")
         xpand.write('Please note that this app can only be used for project demonstration.  It is not authorised by Samsung.')
