8293ce32cd508fe1eaf024124ff8cee495a55de0
Chris Black <31255096+Ckoria@users.noreply.github.com>
2023-06-04 10:19:29 +0200

    Update accessToken.py

--- accessToken.py
+++ accessToken.py
@@ -5,5 +5,22 @@ def access_key(model, sn):
         'ipaas_url':'https://eu.ipaas.samsung.com/eu/gcic/CheckWarranty/1.0/ImportSet'
     }
     #Payload
-    data = st.secrets["DATA"]
+    data = {
+        "IsCommonHeader": {
+        "Company": "C720",
+        "AscCode": "1730640",
+        "Lang": "EN",
+        "Country": "ZA",
+        "Pac": "999999920180502152320"
+        },
+        "IvCustomerCode": "",
+        "IvIMEI": "354175089356499",
+        "IvModel": model,
+        "IvSvcOrderNo": "",
+        "IvProductDate": "",
+        "IvPurchaseDate": "",
+        "IvSerialNo": sn,
+        "IvSvcType": "CI",
+        "IvWtyException": ""
+    }
     return api_token['ipaas_key'], api_token['ipaas_url'], data
