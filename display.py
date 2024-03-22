fc51d4c3669d43d45fd3c5fe3eea0b5105f80278
Chris Black <31255096+Ckoria@users.noreply.github.com>
2024-03-23 01:15:41 +0200

    Hide Error MSG

--- display.py
+++ display.py
@@ -4,12 +4,15 @@ from datetime import datetime
 from PIL import Image
 import streamlit as st
 
+# PLEASE NOTE!!!
+# THE CODE OR SCRIPT IS FUNCTIONING EXCEPT THAT THE API KEY IS NO LONGER WORKING
 
 def display_results():
     st.set_page_config(
     page_title="Warranty Checker",
-    page_icon="🧊")
-    st.subheader('Please note that this app can only be used for project demonstration.')
+    page_icon="🧑‍💻",
+    layout="centered")
+    # st.subheader('Please note that this app can only be used for project demonstration.')
     #Import CSS with page styling
     with open('style.css') as f:
         st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
@@ -28,14 +31,15 @@ def display_results():
     with col2:
         sn  = st.text_input("Enter Serial Number")
     # Get json request to GSPN API
-    try:
-        res = ipaas_auth(model, sn).get('Return')
-    except:
-        st.write("Please check your internet connection")
-    return {'wrty_type': res.get('EvWtyType'),'wrty_date': res.get('EvBasicLaborWtyDate'),
-            'produced_date': res.get('EvProductDate')   
-        }   
+    # try:
+        # res = ipaas_auth(model, sn).get('Return')
+    # except:
+        # st.write("Please check your internet connection")
+    # return {'wrty_type': res.get('EvWtyType'),'wrty_date': res.get('EvBasicLaborWtyDate'),
+            # 'produced_date': res.get('EvProductDate')}
+    
 def visuals():
+    # Not being called due to API key no longer exist
     res = display_results()
    #st.divider()
     col1, col2, col3, col4 = st.columns(4)
@@ -53,7 +57,7 @@ def visuals():
                 st.write(f"Ooops! No longer in warranty. Expired on {datetime.now()-date} ago.")
         with col3:
             wrty_date = f"Warranty expires on {date}"
-            st.write(wrty_date.strftime(%d %B %Y))
+            st.write(wrty_date[0:10])
         with col4:
             date = res.get("produced_date")
             p_date = f'Manufactured on {datetime(int(date[0:4]),int(date[4:6]),int(date[6:]))}'
@@ -62,5 +66,9 @@ def visuals():
         xpand.write('Please note that this app can only be used for project demonstration.  It is not authorised by Samsung.')
     except:
         st.write("Provide information above")
+        
+        
 if __name__ == '__main__':
-    visuals()
+    display_results()
+    st.write("PLEASE NOTE!!!")
+    st.write("THE CODE OR SCRIPT IS FUNCTIONING EXCEPT THAT THE API KEY IS NO LONGER WORKING")
